784d14bf6d4aac67f60459982c3e603defd1d736
Danila Kaslana <kallenkkaslana@gmail.com>
2024-07-06 15:59:15 +0300

    1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,28 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-
+import java.io.*;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try(java.io.FileReader reader = new java.io.FileReader(file))
+        {
+            int symbol;
+            String profile = "";
+            while ((symbol = reader.read()) != -1)
+            {
+                profile += (char)symbol;
+            }
+            profile = profile.replaceAll(".*: ","");
+            String[] data = profile.split("\r\n");
+             return new Profile(data[0],Integer.valueOf(data[1]),data[2],Long.valueOf(data[3]));
+        }
+        catch (IOException exception)
+        {
+            exception.printStackTrace();
+            return new Profile();
+        }
     }
+
 }
